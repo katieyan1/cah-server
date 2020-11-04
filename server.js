40daa4dc44097608babcede8eefddfd87ebecb05
Katie Yan <katieyan12345@gmail.com>
2020-11-04 03:57:23 -0600

    makeid changed

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ const CahGame = require('./game/cah');
 app.use(cors());
 function makeid(length) {
   var result = '';
-  var characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
   var charactersLength = characters.length;
   for (var i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
